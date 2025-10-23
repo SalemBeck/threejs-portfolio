84fb27f0da42ecec350cf782aa08d2f5d8a7e035
salem <salembakhouche42@gmail.com>
2025-10-24 00:38:58 +0100

    fixed the earth position

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,23 @@
 import { Earth } from "./assets/components/earth.jsx";
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import './app.css' 
+import { Float } from '@react-three/drei'
 
 export default function App(){
     return(
   <Canvas camera={{ position: [3, 3, 3] }}>
         <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} />
-        <Earth />
-        <OrbitControls />
+        <ambientLight position={[5, 5, 5]} />
+        
+        <Float rotationIntensity={3}>
+        <Earth
+          scale={[4,4,4]}
+          rotation={[0.41, 0, 0]} // Earth's 23.5° axial tilt
+          position={[0, -1, 0]}
+        />
+      </Float>
+        {/* <OrbitControls/> */}
       </Canvas>
     )
       
--- portfolio/src/assets/components/earth.jsx
+++ portfolio/src/assets/components/earth.jsx
@@ -1,6 +1,6 @@
 import { useGLTF } from '@react-three/drei'
 
-export function Earth() {
+export function Earth(props) {
   const { scene } = useGLTF('/earth.glb')
-  return <primitive object={scene} />
+  return <primitive object={scene} {...props}/>
 }
